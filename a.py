242794d9fb2448773ee1d9f540e22259ca63f4f0
Deepak Bairagi <51380433+deepakbairagi@users.noreply.github.com>
2019-06-07 18:59:36 +0000

    a.py
    a python program to count characters,lines and word in file

---
+++ a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+chars=0
+lines=0
+words=0
+ab=open("a.txt","r")
+while True:
+	f=ab.read(1)
+	if f:
+		if f=='\n':
+			lines=lines+1 
+			words=words+1 	  
+		elif f==" ":
+			words=words+1
+		chars=chars+1
+	else:
+        	print("file end")
+        	break
+print("Number of words in the string:")
+print(words)
+print("Number of lines in the string:")
+print(lines)
+print("Number of characters in the string:")
+print(chars)
+ab.close()
