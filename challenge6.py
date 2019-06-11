72bc74e89a1bd3e38c0bdbcf8d5e18eb297edf95
Deepak Bairagi <51380433+deepakbairagi@users.noreply.github.com>
2019-06-11 21:36:01 +0000

    a program to perform the task performed by cat command

---
+++ challenge6.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+x=int(input(("Enter an option from following to execute cat command:\n1.Print the data of a single file\n2.Print data of multiple files\n3.Create a new empty file\n4.Create a file and enter some data\n5.Append data to already created file\n6.Redirect content of one file and append to an existing file\n"))) 
+if x==1:
+	floc=input("enter name with location:\n")
+	myfile=open(floc,"r")
+	print(myfile.read())
+	myfile.close()
+elif x==2:
+	loc=[]
+	fno=int(input("enter nos. of file to read"))
+	for i in range(fno):
+		fnloc=input("enter name of file "+str(i)+" with location:\n")
+		loc.append(fnloc)
+	for lo in loc:
+		myfile1=open(lo,"r")
+		print(myfile1.read())
+		myfile.close()
+elif x==3:
+	fl=input("enter name of file") 
+	myfile=open(fl,"w")
+	myfile.close()
+elif x==4:
+	fl=input("enter name of file which you want to create and write")
+	myfile=open(fl,"w")
+	print("enter the data to write in file")
+	while True:
+		data=input()
+		if data=='~':
+			break
+		else:
+			myfile.write(data)
+	myfile.close()
+#elif x==5:
+	
+	
+
+
