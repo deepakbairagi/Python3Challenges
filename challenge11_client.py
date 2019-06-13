5560ea541c331f620b758d3cbd69e1a033791dcb
Deepak Bairagi <51380433+deepakbairagi@users.noreply.github.com>
2019-06-13 20:17:32 +0000

    socket programming program for client

---
+++ challenge11_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import socket
+recv_ip="192.168.174.142"
+recv_port=4647
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind((recv_ip,recv_port))
+while 1<2:
+	data=s.recvfrom(150)
+	if data[0]=='$':
+		print("connection terminated")
+		s.close()
+		break
+	else:
+		
+		print("message received from sender::",data[0])
+		print("sender IP + socket::",data[1])
+		reply=raw_input("enter your reply here:-")
+        	if len(reply) > 150:
+			print("plz send reply in 150 characters")
+		else:
+			s.sendto(reply,data[1])
+
